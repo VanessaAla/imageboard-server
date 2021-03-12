957aa4e5f061ad35b8fef7ac158e577eb2ee21ca
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-12 17:33:47 +0100

    imported middleware to the router

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { Router } = require("express");
 const { toJWT, toData } = require("../auth/jwt");
+const authMiddleware = require("../auth/middleware");
 const User = require("../models").user;
 
 const router = new Router();
@@ -12,15 +13,15 @@ router.post("/login", async (req, res, next) => {
       .status(404)
       .send({ message: "Please provide a valid email address and password" });
   } else {
-    const user = await User.findOne({ where: { email: email } });
+    const user = await User.findOne({ where: { email: email } }); //look for the user by email
     if (!user) {
       res.status(404).send({ message: "User not found" });
     } else {
-      console.log("user.password ", user.password);
-      console.log("password test ", password);
+      //console.log("user.password ", user.password);
+      //console.log("password test ", password);
       const userPassword = user.password;
       const result = bcrypt.compareSync(password, userPassword);
-      console.log("result test: ", result);
+      //console.log("result test: ", result);
       if (result) {
         res.send({ jwt: toJWT({ userId: user.id }) });
       } else {
@@ -28,6 +29,12 @@ router.post("/login", async (req, res, next) => {
       }
     }
   }
+});
+
+router.get("/test-auth", authMiddleware, (req, res) => {
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`,
+  });
 });
 
 module.exports = router;
